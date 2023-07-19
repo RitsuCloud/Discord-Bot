5b9dec9cf0b34c62153a441e124a39af3febadb8
RitsuCloud <chonhinchou@gmail.com>
2023-07-18 19:41:59 -0700

    finally got message to work

--- src/bot.py
+++ src/bot.py
@@ -1,25 +1,51 @@
 import os
 import discord
+from discord import Intents
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD= os.getenv('DISCORD_GUILD')
 
-client = discord.Client(intents=discord.Intents.default())
+INTENTS = discord.Intents.default()
+INTENTS.members = INTENTS.messages = INTENTS.message_content = INTENTS.dm_messages = True
+client = discord.Client(intents=INTENTS)
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    guild = discord.utils.get(client.guilds, name=GUILD)
 
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
 
+    # this just displays its own name, since the bot doesn't have enough permission
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members: \n - {members}')
 
+# doesn't quite work
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to the bot test server!!! :)'
+    )
+
+@client.event
+async def on_message(message):
+    print(message.content)
+    if message.author == client.user:
+        return
+    
+    cringe = "SENTINENLSSS OMG LOL"
+
+    if message.content == "val!":
+        response = cringe
+        await message.channel.send(response)
+    
+    if message.content == "!steve":
+        response = cringe
+        await message.channel.send("uh oh stinky")
+        
 client.run(TOKEN)
