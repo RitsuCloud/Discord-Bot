a8d9f1267b2c35ec782bf2a6e53ba6943841f65d
RitsuCloud <chonhinchou@gmail.com>
2023-07-18 21:44:54 -0700

    converted it from client to bot

--- src/bot.py
+++ src/bot.py
@@ -3,6 +3,7 @@ import discord
 import random
 from discord import Intents
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -10,41 +11,31 @@ GUILD= os.getenv('DISCORD_GUILD')
 
 INTENTS = discord.Intents.default()
 INTENTS.members = INTENTS.messages = INTENTS.message_content = INTENTS.dm_messages = True
-client = discord.Client(intents=INTENTS)
 
-@client.event
+bot = commands.Bot(command_prefix="!", intents=INTENTS)
+
+@bot.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
+    print("Bot is READY!!!")
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+@bot.command(name="commands", help="This prints out the list of avaiable commands")
+async def print_commands(ctx):
+    res = "List of Commands : \n" 
+    for command in ALL_COMMENDS:
+        res += command + "\n"
+    await ctx.send(res)
 
-@client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to the bot test server!!! :)'
-    )
+@bot.command(name="luck", help="This tells you your luck for the day!")
+async def luck_today(ctx):
+    res = "Your luck today is : " + random.choice(luck)
+    await ctx.send(res)
 
-@client.event
+@bot.event
 async def on_message(message):
-    
-    if message.author == client.user:
-        return
-
-    if message.content == "!help" or message.content == "!commands":
-        res = "List of Commands : \n" 
-        for command in ALL_COMMENDS:
-            res += command + "\n"
-        await message.channel.send(res)
-
-    if message.content == "!luck":
-        res = "Your luck today is : " + random.choice(luck)
-        await message.channel.send(res)
+    if "fuck" in message.content:
+        await message.channel.send("NO CURSING") 
 
 ALL_COMMENDS = ["!help","!commands","!luck"]
 luck = ["Good", "AWESOME", "Meh", "Pretty bad", "You should probably stay home"]
 
-client.run(TOKEN)
+bot.run(TOKEN)
