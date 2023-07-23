8bcc01b1e01780bee9beeb3808b7e1841c799b05
RitsuCloud <chonhinchou@gmail.com>
2023-07-23 14:55:47 -0700

    added example.txt and main function for textGame.py and it seems to read and parse the file fine

--- src/textGame.py
+++ src/textGame.py
@@ -1,1 +1,16 @@
 # this file is where the game and file parsing will be at
+
+def main():
+  file_path = "text-game/example.txt"
+
+  with open(file_path, 'r') as file:
+      # Step 2: Read the file contents into a variable
+      file_contents = file.read()
+
+  # Step 3: Parse the file contents (Example: Printing each line)
+  lines = file_contents.split("\n")  # Split the content into lines
+  for line in lines:
+      print(line)
+
+if __name__ == "__main__":
+    main()
