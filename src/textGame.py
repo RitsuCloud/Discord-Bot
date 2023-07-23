fb050dc30168296f42ce16cfb49fa2a9bdbf0eaa
RitsuCloud <chonhinchou@gmail.com>
2023-07-23 16:00:36 -0700

    decided what data structure to use for the text game

--- src/textGame.py
+++ src/textGame.py
@@ -1,16 +1,29 @@
-# this file is where the game and file parsing will be at
+"""
+Each block of the game will be in the form of
+#1 number
+description
+options # with their corresponding number to jump to 
+
+this will probably require a struct and hashmap
+"""
+
+lines = []
 
 def main():
-  file_path = "text-game/example.txt"
+  text_parsing("")
 
-  with open(file_path, 'r') as file:
-      # Step 2: Read the file contents into a variable
-      file_contents = file.read()
+def text_parsing(num):
+    file_path = "text-game/example.txt" + num
 
-  # Step 3: Parse the file contents (Example: Printing each line)
-  lines = file_contents.split("\n")  # Split the content into lines
-  for line in lines:
-      print(line)
+    with open(file_path, 'r') as file:
+        # Step 2: Read the file contents into a variable
+        file_contents = file.read()
+
+    # Step 3: Parse the file contents (Example: Printing each line)
+    lines = file_contents.split("\n")  # Split the content into lines
+    for line in lines:
+        if not line[0] == "-":
+            print(line)
 
 if __name__ == "__main__":
     main()
